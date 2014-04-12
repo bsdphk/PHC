94115cce314551f60141568211d3958375a1aef6
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2014-04-12 13:39:06 +0200

    Catfish withdrawn

--- Catfish/ref-c/catfish.h
+++
@@ -1,9 +0,0 @@
-#ifndef CATFISH_H
-#define CATFISH_H
-
-int catfish(uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen, const uint8_t *salt, size_t saltlen, unsigned int t_cost, unsigned int m_cost);
-
-#define PHS(out, outlen, in, inlen, salt, saltlen, t_cost, m_cost) \
-    catfish((uint8_t *)(out), (outlen), (uint8_t *)(in), (inlen), (uint8_t *)(salt), (saltlen), (t_cost), (m_cost))
-
-#endif  // CATFISH_H
--- Catfish/ref-c/pkhash_1024.h
+++
@@ -1,6 +0,0 @@
-#ifndef PKHASH_1024_H
-#define PKHASH_1024_H
-
-void pkhash_slow(const uint8_t *in, const size_t inlen, uint8_t *out);
-
-#endif  // PKHASH_1024_H
